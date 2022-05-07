1e99a2ed2f25e9ada26a6009beaca7018084e4cd
Ruairi O Brien <ruairi@joinzoe.com>
2022-05-07 16:40:01 +0100

    Update to match template

--- app/root.tsx
+++ app/root.tsx
@@ -6,152 +6,50 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch,
 } from "remix";
-import type { LinksFunction } from "remix";
+import { json } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
 
-import globalStylesUrl from "~/styles/global.css";
-import darkStylesUrl from "~/styles/dark.css";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
 
-// https://remix.run/api/app#links
-export let links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: globalStylesUrl },
-    {
-      rel: "stylesheet",
-      href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)",
-    },
-  ];
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
 };
 
-// https://remix.run/api/conventions#default-export
-// https://remix.run/api/conventions#route-filenames
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Ruairi's Site",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    user: await getUser(request),
+  });
+};
+
 export default function App() {
   return (
-    <Document>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </Document>
-  );
-}
-
-// https://remix.run/docs/en/v1/api/conventions#errorboundary
-export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
-  return (
-    <Document title="Error!">
-      <Layout>
-        <div>
-          <h1>There was an error</h1>
-          <hr />
-          <p>Really sorry about this. I'm not sure what happened.</p>
-          <Link to="/">Want to try go to the homepage?</Link>
-        </div>
-      </Layout>
-    </Document>
-  );
-}
-
-// https://remix.run/docs/en/v1/api/conventions#catchboundary
-export function CatchBoundary() {
-  let caught = useCatch();
-
-  let message;
-  switch (caught.status) {
-    case 401:
-      message = (
-        <p>
-          Oops! Looks like you tried to visit a page that you do not have access
-          to.
-          <br />
-          <Link to="/">Want to try go back to the homepage?</Link>
-        </p>
-      );
-      break;
-    case 404:
-      message = (
-        <p>
-          Oops! Looks like you tried to visit a page that does not exist. <br />
-          <Link to="/">Want to try go to the homepage?</Link>
-        </p>
-      );
-      break;
-
-    default:
-      throw new Error(caught.data || caught.statusText);
-  }
-
-  return (
-    <Document title={`${caught.status} ${caught.statusText}`}>
-      <Layout>
-        <h1>
-          {caught.status}: {caught.statusText}
-        </h1>
-        {message}
-      </Layout>
-    </Document>
-  );
-}
-
-function Document({
-  children,
-  title,
-}: {
-  children: React.ReactNode;
-  title?: string;
-}) {
-  return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="h-full">
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        <LiveReload />
       </body>
     </html>
-  );
-}
-
-function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="remix-app">
-      <header className="remix-app__header">
-        <div className="container remix-app__header-content">
-          <Link to="/" title="Remix" className="remix-app__header-home-link">
-            Ruairí's Site
-          </Link>
-          <nav aria-label="Main navigation" className="remix-app__header-nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/posts">Posts</Link>
-              </li>
-              <li>
-                <Link to="/timeline">Timeline</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div className="remix-app__main">
-        <div className="container remix-app__main-content">{children}</div>
-      </div>
-      <footer className="remix-app__footer">
-        <div className="container remix-app__footer-content">
-          <p>&copy; Ruairí O'Brien</p>
-        </div>
-      </footer>
-    </div>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "remix";
+import { Link } from "@remix-run/react";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { useOptionalUser } from "~/utils";
 
 export let meta: MetaFunction = () => {
   return {
@@ -8,11 +10,46 @@ export let meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const user = useOptionalUser();
   return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome!</h2>
-      </main>
-    </div>
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl"></div>
+          <h2>Welcome!</h2>
+          <ul>
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+            <li>
+              <Link to="/timeline">Timeline</Link>
+            </li>
+          </ul>
+          {user ? (
+            <Link
+              to="/notes"
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+            >
+              View Notes for {user.email}
+            </Link>
+          ) : (
+            <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+              <Link
+                to="/join"
+                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+              >
+                Sign up
+              </Link>
+              <Link
+                to="/login"
+                className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
+              >
+                Log In
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
   );
 }
