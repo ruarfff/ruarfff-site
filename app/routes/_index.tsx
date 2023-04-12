902a0dd731dde8a5497696f5c7e6e759e3fd8533
Ruairí O'Brien <rtob@pm.me>
2023-04-12 18:14:46 +0100

    Fix netlify settings

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,24 @@ export const loader = async () => {
   return getPosts();
 };
 
-export default function Posts() {
+export function headers({
+  loaderHeaders,
+  parentHeaders,
+}: {
+  loaderHeaders: Headers;
+  parentHeaders: Headers;
+}) {
+  console.log(
+    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
+  );
+  return {
+    // This is an example of how to set caching headers for a route
+    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
+    "Cache-Control": "public, max-age=60, s-maxage=60",
+  };
+}
+
+export default function Index() {
   let posts = useLoaderData<Post[]>();
   return (
     <div className="container mx-auto max-w-4xl px-4 py-8">
