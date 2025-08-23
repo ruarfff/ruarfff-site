be44cf30e62fd84b9193ae9bf40a3f37bda43029
Ruairí O'Brien <rtob@pm.me>
2025-08-23 14:39:06 -0500

    Tidy up netlify config

--- server.js
+++
@@ -1,7 +0,0 @@
-import * as build from "virtual:react-router/server-build";
-import { createRequestHandler } from "@react-router/node";
-
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   server: {
